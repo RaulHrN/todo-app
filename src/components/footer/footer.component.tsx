36695ee2ac773bda6660d7efe0a25dd477c61d53
Raul <raulnascimento.contato@gmail.com>
2024-08-05 16:15:29 -0300

    footer created

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
+import styles from './footer.module.css';
 
-export const  Footer = () => {
-    return(
-        <div>Footer</div>
-    )
-}
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footer_content}>To do App</p> @ 2024
+    </footer>
+  );
+};
