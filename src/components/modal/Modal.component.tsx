f53aae56e84ddba2c13b75012b981135543a1eb3
Raul <raulnascimento.contato@gmail.com>
2024-08-13 14:10:19 -0300

    Modal updated to material ui modal

--- src/components/modal/Modal.component.tsx
+++ src/components/modal/Modal.component.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 // Styles
-import styles from "./modal.module.css";
+import styles from './modal.module.css';
+
+import { Box, Modal } from '@mui/material';
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
 }
 
-export const Modal = (props: Props) => {
+export const BasicModal = ({ children, open, onClose }: Props) => {
   return (
-  <div className={styles.modal}>
-    <div className={styles.modal_body}>
-        {props.children}
-    </div>
-    </div>
-  )
-}
+    <Modal open={open} onClose={onClose} aria-labelledby="modal-title" aria-describedby="modal-description">
+      <Box className={styles.modal_body}>{children}</Box>
+    </Modal>
+  );
+};
